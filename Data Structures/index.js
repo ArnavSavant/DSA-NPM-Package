d45a7172fea991407138557e354a9a1c57cd3351
Arnav Savant <savantishu02@gmail.com>
2023-05-12 14:53:55 +0530

    Added SinglyLinkedList, BinarySearchTree

---
+++ Data Structures/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+   SinglyLinkedList : require('./SinglyLinkedList'),
+   BinarySearchTree : require('./BinarySearchTree'),
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+const DataStructure = require('./Data Structures/index');
+const Algorithms = require('./Algorithms/index');
+
+module.exports = {
+   DataStructure,
+   Algorithms
+}
